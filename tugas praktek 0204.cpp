183c6d9c74f67992906410848bd46d834e23a291
cindyvitriaa <cindyvitria99@gmail.com>
2017-10-24 19:24:34 +0700

    menampilkan luas 1 box parket

--- tugas praktek 0204.cpp
+++ tugas praktek 0204.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
 	
 	int LR=42;
-	cout<<"Luas Rumah		: "<<LR<<" m2"<<endl;
-	
+	cout<<"Luas Rumah			:"<<LR<<"m2"<<endl;
+	double LP=1.87596;
+	cout<<"Luas 1 box parket	:"<<LP<<"m2"<<endl:
 }
